1fac14b317b16921fc9ee90cfcb871875ef09ce7
Osho957 <oshorajneesh453@gmail.com>
2024-01-07 16:00:07 +0530

    Implemented config server and implemented support of circuit breaker , retry and rate limiter and done testing with jmeter

--- user-service/src/main/java/com/osho/userservice/controller/UserController.java
+++ user-service/src/main/java/com/osho/userservice/controller/UserController.java
@@ -2,13 +2,18 @@ package com.osho.userservice.controller;
 
 import com.osho.userservice.entities.User;
 import com.osho.userservice.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/users")
@@ -16,6 +21,7 @@ import java.util.List;
 public class UserController {
 
     private final  UserService userService;
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     // TODO: modify with dto class after completion
     @PostMapping
@@ -24,8 +30,14 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
     }
 
+    int retryCount = 1;
     @GetMapping("/{userId}")
+//    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallBack")
+//    @Retry(name = "ratingHotelService",fallbackMethod = "ratingHotelFallBack")
+    @RateLimiter(name = "userRateLimiter", fallbackMethod = "ratingHotelFallBack")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId){
+        logger.info("retry count is {}",retryCount);
+        retryCount++;
         User user = userService.getUser(userId);
         return ResponseEntity.status(HttpStatus.OK).body(user);
     }
@@ -34,6 +46,21 @@ public class UserController {
     public ResponseEntity<List<User>> getAllUsers(){
         List<User> users = userService.getAllUser();
         return ResponseEntity.status(HttpStatus.OK).body(users);
+    }
+
+    /*
+          the return type should be similar as of the method from which the circuit breaker fallback is called
+     */
+
+    public ResponseEntity<User> ratingHotelFallBack(String userId, Exception exception){
+//        logger.info("Fallback is executed because service is down :  {} ", exception.getMessage());
+        User user = User.builder()
+                .userId("faulty_user_989")
+                .name("Faulty User")
+                .email("faulty@gmail.com")
+                .about("Returning this because some services are down")
+                .build();
+        return new ResponseEntity<>(user,HttpStatus.BAD_REQUEST);
     }
 
 /*
--- user-service/src/main/java/com/osho/userservice/entities/User.java
+++ user-service/src/main/java/com/osho/userservice/entities/User.java
@@ -1,7 +1,10 @@
 package com.osho.userservice.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +12,9 @@ import java.util.List;
 @Entity
 @Table(name = "users")
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     @Column(name = "ID")
