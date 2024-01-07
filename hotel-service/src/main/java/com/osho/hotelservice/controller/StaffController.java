c8c724c53d5ab41c2fe32b881208865fad7ca0d8
Osho957 <oshorajneesh453@gmail.com>
2024-01-07 13:54:37 +0530

    Implmented Api Gateway and registered all services within it

---
+++ hotel-service/src/main/java/com/osho/hotelservice/controller/StaffController.java
@@ -0,0 +1,22 @@
+package com.osho.hotelservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/staffs")
+public class StaffController {
+
+//    TODO : create crud apis for staffs
+    @GetMapping
+    public ResponseEntity<List<String>> getStaffs() {
+        List<String> list = Arrays.asList("Employee1", "Employee2", "Employee3", "Employee4");
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+}
