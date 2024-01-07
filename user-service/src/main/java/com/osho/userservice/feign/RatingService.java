87b63da30c075fbe44fb3171f01c144214f2aff2
Osho957 <oshorajneesh453@gmail.com>
2024-01-07 12:44:42 +0530

    Adapted Builder Pattern in Rating Class and written a test to verify feign client of rating service

--- user-service/src/main/java/com/osho/userservice/feign/RatingService.java
+++ user-service/src/main/java/com/osho/userservice/feign/RatingService.java
@@ -3,11 +3,13 @@ package com.osho.userservice.feign;
 import com.osho.userservice.entities.Rating;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
+@Service
 @FeignClient(name = "RATING-SERVICE")
 public interface RatingService {
 
--- user-service/src/test/java/com/osho/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/osho/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,32 @@
 package com.osho.userservice;
 
+import com.osho.userservice.entities.Rating;
+import com.osho.userservice.feign.RatingService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserServiceApplicationTests {
 
+	@Autowired
+	private RatingService ratingService;
+
 	@Test
 	void contextLoads() {
 	}
 
+	/*
+	   test to check if feign client for rating service is working or not
+	 */
+	@Test
+	void createRating(){
+		Rating rating = Rating.builder()
+				.rating(9)
+				.userId("userid")
+				.hotelId("hotel id")
+				.feedback("Testing feign client")
+				.build();
+		ratingService.createRating(rating);
+	}
 }
